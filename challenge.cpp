cddde9dd57f33da9f40bd915e870e720d4284622
Wulandary14 <Moonywulan14@gmail.com>
2026-03-02 22:16:16 +0700

    Menambahkan fungsi

--- challenge.cpp
+++ challenge.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 //deklarasi variabel global
 int D1, D2, luas;
+
+//implementasi procedure
+void input(){
+    cout << "masukkan diagonal 1 : ";
+    cin >> D1;
+    cout << "masukkan diagonal 2 : ";
+    cin >> D2;
+}
+
+//implementasi fungsi
+int ProsedureHitungLuas(int D1, int D2){
+    return 0.5 * D1 * D2;
+}
