3fea5ffc91bcb4f665ef75a425d813c99cb62408
Wulandary14 <Moonywulan14@gmail.com>
2026-03-02 22:25:06 +0700

    menambahkan procedure

--- challenge.cpp
+++ challenge.cpp
@@ -16,3 +16,14 @@ void input(){
 int ProsedureHitungLuas(int D1, int D2){
     return 0.5 * D1 * D2;
 }
+
+//implementasi procedure
+void output(){
+    cout << "Luas Belah Ketupat : " << ProsedureHitungLuas(D1, D2) << endl;
+}
+
+int main(){
+    input ();
+    output ();
+    return 0;
+}
